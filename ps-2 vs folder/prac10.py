9318c02ba0fe26720e223412a05ad0a47c3db118
shrikhandeatharva <121312199+shrikhandeatharva@users.noreply.github.com>
2024-05-17 00:46:17 +0530

    Update prac10.py

--- ps-2 vs folder/prac10.py
+++ ps-2 vs folder/prac10.py
@@ -33,3 +33,16 @@ print(correct_predictions)
 
 print("\nIncorrect Predictions:")
 print(incorrect_predictions)
+
+
+# K-means clustering is a popular unsupervised learning algorithm used to partition data into k distinct clusters based on their features. 
+# It iteratively assigns data points to the nearest cluster centroid and updates the centroids until convergence, aiming to 
+# minimize the within-cluster variance. K-means is versatile and widely used in various domains for tasks such as customer 
+# segmentation, image compression, and anomaly detection.
+
+# Algorithm:
+
+# Initialize k cluster centroids randomly.
+# Assign each data point to the nearest centroid based on Euclidean distance.
+# Update the centroids by calculating the mean of all data points assigned to each cluster.
+# Repeat steps 2 and 3 until convergence, where centroids no longer change significantly or a predefined number of iterations is reached.
